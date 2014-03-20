1eb85b3893dd855a8e472dcaf8ee8d6e4e53cba6
Ryan Drost <rdrost@nerdery.com>
2014-03-19 22:58:41 -0500

    scopes for bets part of

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :bets_created, class_name: :Bet, foreign_key: :init_user
+  has_many :bets_challenged, class_name: :Bet, foreign_key: :against_user
+  has_many :bets_won, class_name: :Bet, foreign_key: :winner
+  has_many :bets_lost, class_name: :Bet, foreign_key: :loser
+
 end
